95f29e0f02f1b7a574a3323aa3af56deaee8c503
DryBison <133424845+DryBison@users.noreply.github.com>
2025-12-03 09:57:32 -0600

    Correct import and set base path in vite config
    Fix typo in import statement and update base path.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'ite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ebook-reader-app/'  // Must match your repo name exactly
 })
